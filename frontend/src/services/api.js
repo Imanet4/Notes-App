c56f02abf9e1e0d8ecb4e15ef64caf3fd1f7dbfd
Imanet4 <imanetaifour2@gmail.com>
2025-08-01 12:34:15 +0100

    Centralizing all note-related API calls for easy reuse in my REACT app

---
+++ frontend/src/services/api.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+
+//Creating a pre-config instance of axios with my base URL
+const API = axios.create({
+  baseURL: "http://localhost:8000/api",
+});
+
+
+
+// === API Functions ===
+
+//Fetching all notes from backend
+const fetchNotes = () => API.get("/notes");
+
+//Creating a new note 
+const createNote = (note) => API.post("/notes", note);
+
+//Updating an existing note by ID, & sending the updated data
+const updateNote = (id, note) => API.put(`/notes/${id}`, note);
+
+//Deleting a note by its ID
+const deleteNote = (id) => API.delete(`/notes/${id}`);
+
+
+
+export { fetchNotes, createNote, updateNote, deleteNote };
